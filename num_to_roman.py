807b2a7c37d42e12f3fed8a8faa27de44ca4e4d8
Karthick <karthicksmail@gmail.com>
2018-03-15 07:39:46 +0100

    Reduced the number of lines and added comments

--- num_to_roman.py
+++ num_to_roman.py
@@ -1,51 +1,34 @@
 import random
 
+def convert(num, div):
+	conv_dict = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+	
+	rn = ""
+	hund = num // div # When div is 100, 
+	if hund == 9:
+		rn = rn + (conv_dict[div] + conv_dict[div * 10]) # this is 'CM',
+		hund = 0
+	elif hund >= 5:
+		rn = rn + (conv_dict[div * 5]) # this is 'D',
+		hund = hund - 5
+	elif hund == 4:
+		rn = rn + (conv_dict[div] + conv_dict[div * 5]) # this is 'CD',
+		hund = 0
+	rn = rn + hund * (conv_dict[div])# and this is 'C'
+	num = num % div
+	return (rn, num)
+
 def conv(num):
 	orig_num = num
-
-	conv_dict = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
 
 	thou = num // 1000
 	roman_num = thou * 'M'
 	num = num % 1000
 
-	hund = num // 100
-	if hund == 9:
-		roman_num = roman_num + 'CM'
-		hund = 0
-	elif hund >= 5:
-		roman_num = roman_num + 'D'
-		hund = hund - 5
-	elif hund == 4:
-		roman_num = roman_num + 'CD'
-		hund = 0
-	roman_num = roman_num + hund * 'C'
-	num = num % 100
-	
-	tens = num // 10
-	if tens == 9:
-		roman_num = roman_num + 'XC'
-		tens = 0
-	elif tens >= 5:
-		roman_num = roman_num + 'L'
-		tens = tens - 5
-	elif tens == 4:
-		roman_num = roman_num + 'XL'
-		tens = 0
-	roman_num = roman_num + tens * 'X'
-	num = num % 10
-
-	ones = num
-	if ones == 9:
-		roman_num = roman_num + 'IX'
-		ones = 0
-	elif ones >= 5:
-		roman_num = roman_num + 'V'
-		ones = ones - 5
-	elif ones == 4:
-		roman_num = roman_num + 'IV'
-		ones = 0
-	roman_num = roman_num + ones * 'I'
+	# Start from highest to lowest
+	for div in [100, 10, 1]:
+		(rn, num) = convert(num, div)
+		roman_num = roman_num + rn
 
 	#print(orig_num, roman_num)
 	print(roman_num)
